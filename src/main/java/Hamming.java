22be4b2b7948ca5280d14ba12e308fdcf6cdd9e5
Hulst,Joep J.G.M. van <joep.vanhulst@student.fontys.nl>
2019-04-03 14:57:02 +0200

    Hamming working

--- src/main/java/Hamming.java
+++ src/main/java/Hamming.java
@@ -12,30 +12,12 @@ public class Hamming
          return data;
     }
 
-    public BitSet HammingEncode(String inputstring)
-    {
-        BitSet input = ConvertToBitset(inputstring);
-        BitSet output = new BitSet();
-        printBitSet(input);
-
-        for(int i = 0; i<input.length(); i += 8)
-        {
-            System.out.println("start index: "+ i);
-            BitSet temp = input.get(i,i+7);
-            temp = Hamming7(temp);
-            printBitSet(temp);
-            output = ConcatBitSet(output,temp);
-        }
-        return output;
-    }
-
     private BitSet ConcatBitSet(BitSet input, BitSet toConcat)
     {
         int StartIndex = input.length();
         int EndIndex = toConcat.length();
-        System.out.println(StartIndex);
         BitSet output = new BitSet(StartIndex+EndIndex);
-        for(int i = 0;i<StartIndex;i++)
+        for(int i = 0;i>StartIndex;i++)
         {
             output.set(i,input.get(i));
         }
@@ -53,20 +35,20 @@ public class Hamming
         BitSet parity3 = new BitSet();
         BitSet parity4 = new BitSet();
 
-        for(int current = 0; current<data7.length();current++)
+        for(int current = 0; current<7;current++)
         {
             boolean bit  = data7.get(current);
             switch(current+1)
             {
-                case 1:
+                case 7:
                     parity1.set(1,bit);
                     parity2.set(1,bit);
                     break;
-                case 2:
+                case 6:
                     parity1.set(2,bit);
                     parity3.set(1,bit);
                     break;
-                case 3:
+                case 5:
                     parity2.set(2,bit);
                     parity3.set(2,bit);
                     break;
@@ -75,50 +57,228 @@ public class Hamming
                     parity2.set(3,bit);
                     parity3.set(3,bit);
                     break;
-                case 5:
+                case 3:
                     parity1.set(4,bit);
                     parity4.set(1,bit);
                     break;
-                case 6:
+                case 2:
                     parity2.set(4,bit);
                     parity4.set(2,bit);
                     break;
-                case 7:
+                case 1:
                     parity1.set(5,bit);
                     parity2.set(5,bit);
                     parity4.set(3,bit);
                     break;
             }
         }
-        boolean p1 = parity1.cardinality()%2 == 0;
-        boolean p2 = parity2.cardinality()%2 == 0;
-        boolean p3 = parity3.cardinality()%2 == 0;
-        boolean p4 = parity4.cardinality()%2 == 0;
+        boolean p1 = parity1.cardinality()%2 != 0;
+        boolean p2 = parity2.cardinality()%2 != 0;
+        boolean p3 = parity3.cardinality()%2 != 0;
+        boolean p4 = parity4.cardinality()%2 != 0;
 
         BitSet output = new BitSet();
 
-        output.set(0,p1);
-        output.set(1,p2);
-        output.set(2,data7.get(0));
-        output.set(3,p3);
-        output.set(4,data7.get(1));
-        output.set(5,data7.get(2));
-        output.set(6,data7.get(3));
-        output.set(7,p4);
-        output.set(8,data7.get(4));
-        output.set(9,data7.get(5));
-        output.set(10,data7.get(6));
-
+        output.set(10,p1);
+        output.set(9,p2);
+        output.set(8,data7.get(6));
+        output.set(7,p3);
+        output.set(6,data7.get(5));
+        output.set(5,data7.get(4));
+        output.set(4,data7.get(3));
+        output.set(3,p4);
+        output.set(2,data7.get(2));
+        output.set(1,data7.get(1));
+        output.set(0,data7.get(0));
         return output;
 
 
 
-
     }
 
-    public void printBitSet(BitSet set)
+    public void printBitSet(BitSet set,int length)
     {
-        Gson gson = new Gson();
-        System.out.println("current bits: "+ gson.toJson(set));
+        StringBuilder builder = new StringBuilder();
+        for(int i = length-1;i>=0;i--)
+        {
+            if(set.get(i))
+            {
+                builder.append(1);
+            }
+            else
+            {
+                builder.append(0);
+            }
+        }
+        System.out.println(builder.toString());
     }
+
+    public String hammingDecode(BitSet input)
+    {
+        String output = "";
+        printBitSet(input,11);
+        BitSet paritybits = new BitSet(4);
+        paritybits.set(3,errorCheck(input,1));
+        paritybits.set(2,errorCheck(input,2));
+        paritybits.set(1,errorCheck(input,3));
+        paritybits.set(0,errorCheck(input,4));
+        if(!paritybits.isEmpty())
+        {
+            if(11-(int)convertToLong(paritybits)<0)
+            {
+                return "Error";
+            }
+            else
+            {
+                input.flip(11-(int)convertToLong(paritybits));
+            }
+        }
+        else
+        {
+            System.out.println("Goed");
+            return new String(getData(input).toByteArray());
+        }
+        return new String(getData(input).toByteArray());
+    }
+
+    public long convertToLong(BitSet bits) {
+        int output = 0;
+        if(bits.get(3)){
+            output += 1;
+        }
+        if(bits.get(2)){
+            output += 2;
+        }
+        if(bits.get(1)){
+            output += 4;
+        }
+        if(bits.get(0)){
+            output += 8;
+        }
+        return output;
+    }
+    public Boolean errorCheck(BitSet input,int paritybit)
+    {
+        switch(paritybit)
+        {
+            case 1:
+                BitSet checkSet = new BitSet(5);
+                int index = 0;
+                for(int i = 0;i<11;i++)
+                {
+                    if(i==0|i==2|i==4|i==6|i==8)
+                    {
+                        checkSet.set(index,input.get(i));
+                        index++;
+                    }
+                }
+                return !((checkSet.cardinality()%2!=0) == input.get(10));
+            case 2:
+                checkSet = new BitSet(5);
+                index = 0;
+                for(int i = 0;i<11;i++)
+                {
+                    if(i == 0| i==1|i==4| i==5|i==8)
+                    {
+                        checkSet.set(index,input.get(i));
+                        index++;
+                    }
+                }
+                return !((checkSet.cardinality()%2!=0) == input.get(9));
+            case 3:
+                checkSet = new BitSet(5);
+                index = 0;
+                for(int i = 0;i<11;i++)
+                {
+                    if( i ==4 | i==5|i==6)
+                    {
+                        checkSet.set(index,input.get(i));
+                        index++;
+                    }
+                }
+                return !((checkSet.cardinality()%2 != 0) == input.get(7));
+            case 4:
+                checkSet = new BitSet(5);
+                index = 0;
+                for(int i = 0;i<11;i++)
+                {
+                    if( i ==0 | i==1|i==2)
+                    {
+                        checkSet.set(index,input.get(i));
+                        index++;
+                    }
+                }
+                return !((checkSet.cardinality()%2!=0) == input.get(3));
+            default:
+                return false;
+        }
+    }
+    public Boolean twoErrorCheck(BitSet input)
+    {
+        BitSet newSet = getData(input);
+        BitSet toCheck = Hamming7(newSet);
+        for(int i = 0; i<11;i++)
+        {
+            if(toCheck.get(i)!=input.get(i))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public BitSet getData (BitSet input)
+    {
+        BitSet newSet = new BitSet(7);
+        int currentbit = 0;
+        for(int i = 0;i<11;i++)
+        {
+            if(i!=3 & i!=7 & i!=9&i!=10)
+            {
+                newSet.set(currentbit,input.get(i));
+                currentbit++;
+            }
+        }
+        return newSet;
+    }
+
+    public BitSet addNoise(BitSet input)
+    {
+        for(int i=0;i<11;i++)
+        {
+            if(Math.random()>=0.95)
+            {
+                input.flip(i);
+            }
+
+        }
+        return input;
+//        input.flip(0);
+//        return input;
+    }
+
+    public String hammingEncodewithNoiseString(String string)
+    {
+        StringBuilder output = new StringBuilder();
+        for(char c:string.toCharArray())
+        {
+            System.out.println("Current character to encode: "+c);
+            BitSet original = ConvertToBitset(Character.toString(c));
+            BitSet encoded = Hamming7(original);
+            printBitSet(encoded,11);
+            encoded = addNoise(encoded);
+            String decoded = hammingDecode(encoded);
+            if(decoded != "Error")
+            {
+                output.append(decoded);
+            }
+            else
+            {
+                return "Error in decoding";
+            }
+
+        }
+        return output.toString();
+    }
+
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,9 +4,16 @@ public class Main
 {
     public static void main(String[] args) {
     Hamming hamming = new Hamming();
-    BitSet input = hamming.ConvertToBitset("a");
-    BitSet output = hamming.HammingEncode("joep");
-    hamming.printBitSet(output);
+//    BitSet input = hamming.ConvertToBitset("a");
+//    hamming.printBitSet(input,7);
+//    BitSet output = hamming.Hamming7(input);
+//    hamming.printBitSet(output,11);
+//    output = hamming.addNoise(output);
+//    hamming.printBitSet(output,11);
+//    String test = hamming.hammingDecode(output);
+//    System.out.println(test);
+        System.out.println(hamming.hammingEncodewithNoiseString("test"));
+
     // write your code here
     }
 }
